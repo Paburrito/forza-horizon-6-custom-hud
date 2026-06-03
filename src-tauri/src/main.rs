b71c3a8ae9453c162600033ddcd808b645fe146e
Paburrito <pabloracer_1992@hotmail.com>
2026-06-03 00:44:16 -0400

    Revamped tutorial with improved QoL for first time users (try locking yourself mid-tutorial now lol), added an easy download button to this repo when app gets updated

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -244,6 +244,7 @@ async fn main() {
     // Start Tauri window
     tauri::Builder::default()
         .plugin(tauri_plugin_fs::init())
+        .plugin(tauri_plugin_opener::init())
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
